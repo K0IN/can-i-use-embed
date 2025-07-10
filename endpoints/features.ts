5b371e8dd93e1d679e7fb79bf38d696b657f5173
K0IN <thisk0in@gmail.com>
2025-07-11 00:35:24 +0200

    Remove logging

--- endpoints/features.ts
+++ endpoints/features.ts
@@ -2,7 +2,6 @@ import { Context } from "hono";
 import { getListOfFeatures } from "../caniuse/browsercompat.ts";
 
 export function listAllFeatures(context: Context) {
-  const allVersions = getListOfFeatures();
-  console.log("Browser Compatibility Data:", allVersions);
-  return context.json(allVersions, 200, { "Content-Type": "application/json" });
+    const allVersions = getListOfFeatures();
+    return context.json(allVersions, 200, { "Content-Type": "application/json" });
 }
